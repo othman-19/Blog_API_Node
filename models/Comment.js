e9ec4bdde8a325e60fe8e1318f2aa746adf67d9c
othmane <othman-19@hotmail.fr>
2020-12-06 11:12:35 +0100

    set Comment model and routes

---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+      required: [true, "can't be blank"],
+    },
+    commenter: {
+      name: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/^[a-zA-Z0-9]+$/, 'is invalid'],
+        maxlength: 100,
+      },
+      email: {
+        type: String,
+        lowercase: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+      },
+      maxlength: 200,
+    },
+  },
+  { timestamps: true },
+);
+
+PostSchema.virtual('url').get(function () {
+  return `/posts/${this._id}`;
+});
+
+module.exports = mongoose.model('Post', PostSchema);
--- routes/comments.js
+++ routes/comments.js
@@ -1,14 +1,13 @@
 const commentRouter = require('express').Router();
 const commentController = require('../controllers/commentController');
-const commenterController = require('../controllers/commenterController');
 
 commentRouter.get('/new', commentController.new);
 
-commentRouter.post('/', commenterController.validations, commentController.validations, commenterController.create, commentController.create);
+commentRouter.post('/', commentController.validations, commentController.create);
 
 commentRouter.get('/:id/edit', commentController.edit);
 
-commentRouter.put('/:id', commenterController.validations, commentController.validations, commenterController.create, commentController.update);
+commentRouter.put('/:id', commentController.validations, commentController.update);
 
 commentRouter.delete('/:id', commentController.destroy);
 

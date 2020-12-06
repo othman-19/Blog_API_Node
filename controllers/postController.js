fc8c12fdd7d90181fbf07a5dff4cd23a1bcc5692
othmane <othman-19@hotmail.fr>
2020-12-06 18:38:26 +0100

    add show function to postcontroller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,18 @@
 const { check, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 const Post = require('../models/Post');
+
+exports.validations = [
+  check('title', 'Title must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  check('text', 'Text must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+];
 
 exports.index = (req, res, next) => {
   Post.find({ published: true }, 'title text createdAt')
@@ -8,5 +21,19 @@ exports.index = (req, res, next) => {
         return next(err);
       }
       return res.json(posts);
+    });
+};
+
+exports.show = (req, res, next) => {
+  Post.findById({ _id: mongoose.Types.ObjectId(req.params.id) })
+    .exec()
+    .then(post => {
+      if (!post) {
+        return next({ message: 'The post was not found.' });
+      }
+      return res.json(post);
+    })
+    .catch(err => {
+      res.json(err);
     });
 };

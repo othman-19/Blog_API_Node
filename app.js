7d9cc0f3726cebef376feafce95fa2b8e2905a94
othmane <othman-19@hotmail.fr>
2020-12-09 19:05:37 +0100

    set the rate limit middleware

--- app.js
+++ app.js
@@ -90,6 +90,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  delayMs: 0, // disable delaying - user has full speed until the max limit is reached
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

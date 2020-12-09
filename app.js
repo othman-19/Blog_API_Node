e4ef03f2dcaf2969dcd73d7d91924a8e6983e25d
othmane <othman-19@hotmail.fr>
2020-12-09 19:08:19 +0100

    set the method override middleware

--- app.js
+++ app.js
@@ -96,6 +96,15 @@ app.use(rateLimit({
   delayMs: 0, // disable delaying - user has full speed until the max limit is reached
 }));
 
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
